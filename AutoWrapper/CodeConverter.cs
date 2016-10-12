ae6c4dcb0048bc38bf263ad7ee65bb6cef93877a
BlackJax96 <blackjax0@gmail.com>
2016-10-12 11:51:32 -0700

    no message

---
+++ AutoWrapper/CodeConverter.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoWrapper
+{
+    public static class CodeConverter
+    {
+        public static void Convert(string fileName, string inputDir, string outputDir)
+        {
+            string inPath = inputDir + "\\" + fileName + ".h";
+            string outPathHeader = outputDir + "\\" + fileName + ".h";
+            string outPathCode = outputDir + "\\" + fileName + ".cpp";
+            string origFile = File.ReadAllText(inPath);
+
+            List<string> code = origFile.Split(new string[] { Environment.NewLine, ";" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            Clean(ref code, true, true);
+
+            File.WriteAllText(outPathHeader, string.Join(Environment.NewLine, code));
+        }
+        private static void Clean(ref List<string> input, bool removeComments, bool removeWhitespace)
+        {
+            bool comment = false;
+            for (int i = 0; i < input.Count; ++i)
+            {
+                int index;
+                string s = input[i];
+                if (comment)
+                {
+                    index = s.IndexOf("*/");
+                    if (index >= 0)
+                    {
+                        comment = false;
+                        if (removeComments)
+                        {
+                            index += 2;
+                            if (index >= s.Length - 1)
+                            {
+                                input.RemoveAt(i--);
+                                continue;
+                            }
+                            s = s.Substring(index, s.Length - index);
+                        }
+                    }
+                    else if (removeComments)
+                    {
+                        input.RemoveAt(i--);
+                        continue;
+                    }
+                }
+                index = s.IndexOf("/*");
+                if (index >= 0)
+                {
+                    comment = true;
+                    if (removeComments)
+                    {
+                        if (index == 0)
+                        {
+                            input.RemoveAt(i--);
+                            continue;
+                        }
+                        s = s.Substring(0, index);
+                    }
+                }
+                index = s.IndexOf("//");
+                if (index >= 0)
+                {
+                    if (removeComments)
+                    {
+                        if (index == 0)
+                        {
+                            input.RemoveAt(i--);
+                            continue;
+                        }
+                        s = s.Substring(0, index);
+                    }
+                }
+                if (s.Contains("private:") || s.Contains("protected:"))
+                {
+
+                }
+
+                if (removeWhitespace && String.IsNullOrWhiteSpace(s))
+                    input.RemoveAt(i--);
+                else
+                    input[i] = s;
+            }
+        }
+    }
+
+    public class CMethod
+    {
+        public enum Type
+        {
+            _virtual,
+            _abstract,
+            _override,
+            _inline,
+        }
+
+        public Type _type;
+        public string _returnType;
+    }
+
+    public class CClass
+    {
+        public string _name;
+        public List<CMethod> _publicMethods;
+    }
+}
---
+++ AutoWrapper/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AutoWrapper
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string rootPath;
+            Folder root = null;
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Select the C++ directory";
+            if (fbd.ShowDialog() == DialogResult.OK)
+                root = GetFolders(null, rootPath = fbd.SelectedPath);
+            else
+            {
+                Application.Exit();
+                return;
+            }
+            root._name = null;
+            fbd.Description = "Select the output CLR directory";
+            if (fbd.ShowDialog() == DialogResult.OK)
+                WriteFolders(root, rootPath, fbd.SelectedPath);
+        }
+
+        private static Folder GetFolders(Folder parent, string path)
+        {
+            Folder f = new Folder()
+            {
+                _name = Path.GetFileName(path),
+                _parentFolder = parent
+            };
+            IEnumerable<string> files = Directory.EnumerateFiles(path);
+            foreach (string p in files)
+                if (Path.GetExtension(p).ToLower().Equals(".h"))
+                    f._headerNames.Add(Path.GetFileNameWithoutExtension(p));
+            IEnumerable<string> dirs = Directory.EnumerateDirectories(path);
+            foreach (string dir in dirs)
+                f._childFolders.Add(GetFolders(f, dir));
+            return f;
+        }
+
+        private static void WriteFolders(Folder folder, string inputRootDir, string outputRootDir)
+        {
+            string outputPath = folder.GetFullDirectoryPath(outputRootDir);
+
+            if (!Directory.Exists(outputPath))
+                Directory.CreateDirectory(outputPath);
+
+            foreach (string file in folder._headerNames)
+                CodeConverter.Convert(file, folder.GetFullDirectoryPath(inputRootDir), outputPath);
+
+            foreach (Folder f in folder._childFolders)
+                WriteFolders(f, inputRootDir, outputRootDir);
+        }
+    }
+
+    public class Folder
+    {
+        public string _name;
+        public Folder _parentFolder = null;
+        public List<Folder> _childFolders = new List<Folder>();
+        public List<string> _headerNames = new List<string>();
+
+        public string GetRelativePath()
+        {
+            if (_parentFolder == null)
+                return _name;
+            return _parentFolder.GetRelativePath() + "\\" + _name;
+        }
+
+        public string GetFullDirectoryPath(string rootPath)
+        {
+            if (_name == null)
+                return rootPath;
+            return rootPath + "\\" + GetRelativePath();
+        }
+    }
+}

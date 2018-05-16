db74ac8e9c65e5be4c0d3a82d9c72dc27453c3df
BlackJax96 <blackjax0@gmail.com>
2018-05-15 20:55:16 -0700

    no message

--- Build/Engine/Shaders/Scene3D/IrradianceConvolution.fs
+++ Build/Engine/Shaders/Scene3D/IrradianceConvolution.fs
@@ -8,32 +8,34 @@ layout (binding = 0) uniform samplerCube Texture0; //Environment map
 const float PI = 3.14159265359f;
 
 void main()
-{		
+{
     vec3 N = normalize(FragPos);
 
-    vec3 irradiance = vec3(0.0f);   
-    
+    vec3 irradiance = vec3(0.0f);
+
     // tangent space calculation from origin point
     vec3 up    = vec3(0.0f, 1.0f, 0.0f);
     vec3 right = cross(up, N);
     up         = cross(N, right);
-       
+
     float sampleDelta = 0.025f;
-    int nrSamples = 0;
+    float nrSamples = 0.0f;
     for (float phi = 0.0f; phi < 2.0f * PI; phi += sampleDelta)
     {
         for (float theta = 0.0f; theta < 0.5f * PI; theta += sampleDelta)
         {
             // spherical to cartesian (in tangent space)
-            vec3 tangentSample = vec3(sin(theta) * cos(phi),  sin(theta) * sin(phi), cos(theta));
+            float tanX = sin(theta) * cos(phi);
+            float tanY = sin(theta) * sin(phi);
+            float tanZ = cos(theta);
 
             // tangent space to world
-            vec3 sampleVec = tangentSample.x * right + tangentSample.y * up + tangentSample.z * N; 
+            vec3 sampleVec = tanX * right + tanY * up + tanZ * N;
 
             irradiance += texture(Texture0, sampleVec).rgb * cos(theta) * sin(theta);
             nrSamples++;
         }
     }
 
-    FragColor = vec3(1.0f, 0.0f, 0.0f);//PI * irradiance * (1.0f / float(nrSamples));
+    FragColor = PI * irradiance * (1.0f / float(nrSamples));
 }
